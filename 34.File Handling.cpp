d132f36d37f478ea2245313efaf153953a1a01ab
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-08-28 19:53:52 +0530

    File Handling

--- 34.File Handling.cpp
+++ 34.File Handling.cpp
@@ -7,18 +7,25 @@ int main()
 {
     /*Write content to file*/
     string A = "I am write this to file using file handling";
-    string B;
-   
-    ofstream in("34.File Handle.txt");
+
+    /*Using constructor*/
+    /*ofstream in("34.File Handle.txt");*/
+
+    /*Using open function*/
+    ofstream in;
+    in.open("34.File Handle.txt");
     in << A;
-    in.close();
+    in.close();//Close file
 
+    /*Read from file*/
+    string B;
+    /*Using constructor*/
+    /*ifstream out("34.File Handle.txt");*/
 
-    ifstream out("34.File Handle.txt");
-     getline(out,B);
-    cout<<B;
-    out.close();
-
-
-
+    /*Using open function*/
+    ifstream out;
+    out.open("34.File Handle.txt");
+    getline(out, B);
+    cout << B;
+    out.close();//Close file
 }
